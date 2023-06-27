e188138b00c3716d5a53bd103518c9d926d01251
Crowdbotics <team@crowdbotics.com>
2023-06-27 09:09:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flawless-41755",
+  url: "https://flawless-41755.botics.co",
+  api: "https://flawless-41755.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
